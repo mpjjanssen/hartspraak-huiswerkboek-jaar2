40d26225c88a456dab29bae057476d884db43bfb
mpjjanssen <74655649+mpjjanssen@users.noreply.github.com>
2026-02-07 22:19:29 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,18 +7,17 @@ import { ThemeProvider } from "./contexts/ThemeContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import { AdminAuthProvider } from "./contexts/AdminAuthContext";
 import { EncryptionProvider } from "./contexts/EncryptionContext";
-import { TestProvider } from "./contexts/TestContext";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { AdminProtectedRoute } from "./components/AdminProtectedRoute";
 import Home from "./pages/Home";
 import Aandachtspunten from "./pages/Aandachtspunten";
 import Workshop1 from "./pages/Workshop1";
+import Workshop1Day2 from "./pages/Workshop1Day2";
 import Workshop2 from "./pages/Workshop2";
 import Workshop3 from "./pages/Workshop3";
 import Workshop4 from "./pages/Workshop4";
-import Workshop5 from "./pages/Workshop5";
-import Workshop6 from "./pages/Workshop6";
 import Referenties from "./pages/Referenties";
+import DeMaskermaker from "./pages/DeMaskermaker";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -28,10 +27,6 @@ import AdminDashboard from "./pages/AdminDashboard";
 import AdminUsage from "./pages/AdminUsage";
 import AdminSharedAnswers from "./pages/AdminSharedAnswers";
 import Privacy from "./pages/Privacy";
-import TestIntro from "./pages/TestIntro";
-import TestPart1 from "./pages/TestPart1";
-import TestPart2 from "./pages/TestPart2";
-import TestResults from "./pages/TestResults";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 
@@ -56,6 +51,11 @@ function MemberRouter() {
               <Workshop1 />
             </ProtectedRoute>
           )} />
+          <Route path="/workshop/1-dag-2" component={() => (
+            <ProtectedRoute>
+              <Workshop1Day2 />
+            </ProtectedRoute>
+          )} />
           <Route path="/workshop/2" component={() => (
             <ProtectedRoute>
               <Workshop2 />
@@ -71,40 +71,14 @@ function MemberRouter() {
               <Workshop4 />
             </ProtectedRoute>
           )} />
-          <Route path="/workshop/5" component={() => (
-            <ProtectedRoute>
-              <Workshop5 />
-            </ProtectedRoute>
-          )} />
-          <Route path="/workshop/6" component={() => (
-            <ProtectedRoute>
-              <Workshop6 />
-            </ProtectedRoute>
-          )} />
-          {/* Karakterstructuren Test routes */}
-          <Route path="/test" component={() => (
-            <ProtectedRoute>
-              <TestIntro />
-            </ProtectedRoute>
-          )} />
-          <Route path="/test/deel1" component={() => (
-            <ProtectedRoute>
-              <TestPart1 />
-            </ProtectedRoute>
-          )} />
-          <Route path="/test/deel2" component={() => (
-            <ProtectedRoute>
-              <TestPart2 />
-            </ProtectedRoute>
-          )} />
-          <Route path="/test/resultaten" component={() => (
-            <ProtectedRoute>
-              <TestResults />
-            </ProtectedRoute>
-          )} />
           <Route path="/referenties" component={() => (
             <ProtectedRoute>
               <Referenties />
+            </ProtectedRoute>
+          )} />
+          <Route path="/test" component={() => (
+            <ProtectedRoute>
+              <DeMaskermaker />
             </ProtectedRoute>
           )} />
           <Route path="/privacy" component={() => (
@@ -166,12 +140,10 @@ function App() {
         <AuthProvider>
           <AdminAuthProvider>
             <EncryptionProvider>
-              <TestProvider>
-                <TooltipProvider>
-                  <Toaster />
-                  <Router />
-                </TooltipProvider>
-              </TestProvider>
+              <TooltipProvider>
+                <Toaster />
+                <Router />
+              </TooltipProvider>
             </EncryptionProvider>
           </AdminAuthProvider>
         </AuthProvider>
@@ -181,3 +153,4 @@ function App() {
 }
 
 export default App;
+
